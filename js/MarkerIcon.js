83d6473d58cc9862b50b99062e7abb8cf2afd431
David <david.ershag@gmail.com>
2013-07-02 16:27:53 +0200

    Added MarkerIcon.js

---
+++ js/MarkerIcon.js
@@ -0,0 +1,16 @@
+/*global L, define */
+
+
+define(function () {
+
+	"use strict";
+
+	return L.Icon.extend({
+	    options: {
+			iconSize:     [34, 42],
+			iconAnchor:   [3, 35],
+			popupAnchor:  [12, -37]
+	    }
+	});
+
+});
